6d168b75277f803800cd17754657d89a20fc2ddf
Alberto Lopez <betomper@gmail.com>
2020-08-25 22:14:57 -0500

    cambio color version 1

--- app/src/main/java/com/example/prueba/MenuAjustesFragment.kt
+++ app/src/main/java/com/example/prueba/MenuAjustesFragment.kt
@@ -1,12 +1,18 @@
 package com.example.prueba
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.RoundRectShape
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.menu_ajustes_fragment.*
 
@@ -23,14 +29,21 @@ class MenuAjustesFragment : Fragment() {
         return inflater.inflate(R.layout.menu_ajustes_fragment, container, false)
     }
 
-    @SuppressLint("ResourceType")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         view.findViewById<Button>(R.id.CambiarColor).setOnClickListener(){
-            var bgShape = menuAjustes.background;
-            val res = resources;
-            menuAjustes.background = Drawable.createFromXml(res, res.getXml(R.drawable.gradiente));
-            println("hola bb");
+//************************************************************************
+            val gradientDrawable = GradientDrawable(
+                GradientDrawable.Orientation.LEFT_RIGHT,
+                intArrayOf(Color.parseColor("#0d15ff"),
+                    Color.parseColor("#54FFD4"),
+                    Color.parseColor("#0d15ff")
+                    )
+            );
+            gradientDrawable.cornerRadius = 0f;
+            menuAjustes.background = gradientDrawable;
+//****************************************************************************
+
         }
     }
 }
