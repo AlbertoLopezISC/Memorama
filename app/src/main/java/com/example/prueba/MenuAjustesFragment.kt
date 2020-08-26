48dfae46962b2f7f4f6fc209524423a58eebfae5
Alberto Lopez <betomper@gmail.com>
2020-08-26 13:11:47 -0500

    tutorial avance

--- app/src/main/java/com/example/prueba/MenuAjustesFragment.kt
+++ app/src/main/java/com/example/prueba/MenuAjustesFragment.kt
@@ -2,19 +2,21 @@ package com.example.prueba
 
 import android.annotation.SuppressLint
 import android.graphics.Color
-import android.graphics.drawable.Drawable
 import android.graphics.drawable.GradientDrawable
-import android.graphics.drawable.ShapeDrawable
-import android.graphics.drawable.shapes.RoundRectShape
-import android.os.Build
+import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.fragment_first.*
 import kotlinx.android.synthetic.main.menu_ajustes_fragment.*
+import org.w3c.dom.Document
+import java.io.File
+import javax.xml.parsers.DocumentBuilder
+import javax.xml.parsers.DocumentBuilderFactory
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -29,20 +31,43 @@ class MenuAjustesFragment : Fragment() {
         return inflater.inflate(R.layout.menu_ajustes_fragment, container, false)
     }
 
+    @SuppressLint("ResourceType")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         view.findViewById<Button>(R.id.CambiarColor).setOnClickListener(){
 //************************************************************************
             val gradientDrawable = GradientDrawable(
                 GradientDrawable.Orientation.LEFT_RIGHT,
-                intArrayOf(Color.parseColor("#0d15ff"),
+                intArrayOf(
+                    Color.parseColor("#0d15ff"),
                     Color.parseColor("#54FFD4"),
                     Color.parseColor("#0d15ff")
-                    )
+                )
             );
             gradientDrawable.cornerRadius = 0f;
             menuAjustes.background = gradientDrawable;
+
+            //firstFragment.background = gradientDrawable;
+
+
 //****************************************************************************
+//--------------------------------------------------------------
+            /*
+            val fXmlFile = File("drawable/gradiente.xml")
+            println(fXmlFile)
+
+            val dbFactory: DocumentBuilderFactory = DocumentBuilderFactory.newInstance()
+            val dBuilder: DocumentBuilder = dbFactory.newDocumentBuilder()
+            val doc: Document = dBuilder.parse(fXmlFile)
+
+            doc.documentElement.normalize()
+
+            println(doc.documentElement.nodeName)
+
+            val aux = resources.getXml(R.drawable.gradiente);
+             */
+//--------------------------------------------------------------
 
         }
     }
--- app/src/main/java/com/example/prueba/SecondFragment.kt
+++ app/src/main/java/com/example/prueba/SecondFragment.kt
@@ -1,12 +1,19 @@
 package com.example.prueba
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.activity_jugar.*
+import kotlinx.android.synthetic.main.fragment_second.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -19,13 +26,68 @@ class SecondFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false)
+
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        var cont = 0
+        var c1 = false
+        var c2 = false
+        val drawable: Drawable? = androidx.core.content.res.ResourcesCompat.getDrawable(resources, R.drawable.sol, null)
+        Handler().postDelayed(Runnable { // Do something after 5s = 5000ms
+            textViewInstrucciones.requestFocus(1);
+        }, 1000)
         view.findViewById<Button>(R.id.button_regresar).setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+        view.findViewById<Button>(R.id.button_siguiente).setOnClickListener {
+            if(cont == 0){
+                button_siguiente.contentDescription = "Muy bien, ahora apareceran una carta tapada" +
+                        "en el centro de la pantalla. Presiona una para escuchar cual numero de carta es. Presiona dos veces" +
+                        "para destaparla y ver lo que es. Cuando lo hayas hecho, presiona siguiente"
+                view.findViewById<TextView>(R.id.textViewInstrucciones).text = "Muy bien, ahora apareceran una carta tapada" +
+                        "en el centro de la pantalla. Presiona una para escuchar cual numero de carta es. Presiona dos veces" +
+                        "para destaparla y ver lo que es. Cuando lo hayas hecho, presiona siguiente"
+                Handler().postDelayed(Runnable { // Do something after 5s = 5000ms
+                    if(button_siguiente != null){
+                        button_siguiente.contentDescription = "Boton siguiente";
+                    }
+                }, 15000)
+                carta1Instrucciones.visibility = View.VISIBLE;
+                cont++;
+            } else if(cont == 1){
+                button_siguiente.contentDescription = "Muy bien, ahora que ya viste que carta es, aparecera" +
+                        "la siguiente carta y tendras destaparla de nuevo.";
+                view.findViewById<TextView>(R.id.textViewInstrucciones).text = "Muy ahora que ya viste que carta es, aparecera" +
+                        "la siguiente carta, y tendras destaparla de nuevo.";
+                Handler().postDelayed(Runnable { // Do something after 5s = 5000ms
+                    if(button_siguiente != null){
+                        button_siguiente.contentDescription = "Boton siguiente";
+                    }
+                }, 10000)
+                carta2Instrucciones.visibility = View.VISIBLE;
+                cont++;
+            }
+        }
+        view.findViewById<Button>(R.id.carta1Instrucciones).setOnClickListener(){
+            carta1Instrucciones.background = drawable;
+            carta1Instrucciones.contentDescription = "Carta destapada, Sol brillante al amanecer";
+            Handler().postDelayed(Runnable { // Do something after 5s = 5000ms
+                carta1Instrucciones.requestFocus();
+            }, 1000)
+            c1 = true;
+        }
+        carta2Instrucciones.setOnClickListener(){
+            carta2Instrucciones.background = drawable;
+            carta2Instrucciones.contentDescription = "Carta destapada, Sol brillante al amanecer";
+            Handler().postDelayed(Runnable { // Do something after 5s = 5000ms
+                view.findViewById<TextView>(R.id.textViewInstrucciones).text = "Excelente, hemos encontrado un par, ahora" +
+                        "veremos lo que pasa cuando no encontramos un par";
+                carta2Instrucciones.contentDescription = "Excelente, hemos encontrado un par, ahora" +
+                        "veremos lo que pasa cuando no encontramos un par";
+            }, 2500)
         }
     }
 }
